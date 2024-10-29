7e8e8e632c72d8c1cdbbcafa3cb03271f3ca8d4c
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-10-29 17:30:08 -0500

    feat: :rocket: vista

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-const repo = '/GUILLERPORFOLIO/'
+//const repo = '/GUILLERPORFOLIO/'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: repo,
+  base: 'https://guiller022005.github.io/GuillerPorfolio/',
   plugins: [vue()],
 })
