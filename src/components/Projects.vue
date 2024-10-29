25d9cf3fd9e8edc58ff6998988494f2018caa417
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-10-29 17:59:07 -0500

    feat: :sparkles: otra prueba

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -16,30 +16,33 @@
 
 <script setup>
 import { ref } from 'vue';
-
+import cine from '../assets/cine.png';
+import ecomercePng from '../assets/ecomerce.png';
+import spaceXPng from '../assets/spaceX.png';
+import foodSitePng from '../assets/foodSite.png';
 const projects = ref([
   {
     title: 'Plataforma de Cine',
     description: 'Una aplicación web para visualizar el catálogo de películas en cartelera y gestionar reservas.',
-    image: '../assets/cine.png?height=200&width=300',
+    image: cine,
     url: 'https://github.com/Guiller022005/CineCampus',
   },
   {
     title: 'Plataforma de E-commerce',
     description: 'Una solución full-stack de comercio electrónico con gestión de inventario en tiempo real.',
-    image: '../assets/ecomerce.png?height=200&width=300',
+    image: ecomercePng,
     url: 'https://guiller022005.github.io/ecommerCampusM1/?id=aps',
   },
   {
     title: 'SpaceX',
     description: 'Aplicación interactiva que muestra misiones espaciales, lanzamientos y vehículos de SpaceX.',
-    image: '../assets/spaceX.png?height=200&width=300',
+    image: spaceXPng,
     url: 'https://guiller022005.github.io/SpaceX/',
   },
   {
     title: 'Foods',
     description: 'Aplicación interactiva que muestra misiones espaciales, lanzamientos y vehículos de SpaceX.',
-    image: '../assets/foodSite.png?height=200&width=300',
+    image: foodSitePng,
     url: 'https://guiller022005.github.io/proyectoWeb/',
   }
 ]);
